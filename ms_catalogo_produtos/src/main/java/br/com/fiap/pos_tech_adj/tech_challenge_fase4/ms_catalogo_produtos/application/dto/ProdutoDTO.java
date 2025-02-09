c1b5ccaa2161f2fef0dd04955155e922c6050b2d
venancio2000 <jbrunogvenancio@gmail.com>
2025-02-08 22:27:47 -0300

    conversão de tipos em testes de preco de produto

--- ms_catalogo_produtos/src/main/java/br/com/fiap/pos_tech_adj/tech_challenge_fase4/ms_catalogo_produtos/application/dto/ProdutoDTO.java
+++ ms_catalogo_produtos/src/main/java/br/com/fiap/pos_tech_adj/tech_challenge_fase4/ms_catalogo_produtos/application/dto/ProdutoDTO.java
@@ -1,11 +1,14 @@
 package br.com.fiap.pos_tech_adj.tech_challenge_fase4.ms_catalogo_produtos.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
-@Data @NoArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProdutoDTO {
     private Long id;
     private String nome;
