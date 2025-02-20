764a2d3870381aa336b5bb2e0a08091b433a1e08
JulianaOlio <115197866+JulianaOlio@users.noreply.github.com>
2025-02-20 00:19:17 -0300

    Update PedidoControllerTest.java

--- ms_gestao_pedidos/src/test/java/br/com/fiap/pos_tech_adj/tech_challenge_fase4/ms_gestao_pedidos/adapters/in/controller/PedidoControllerTest.java
+++ ms_gestao_pedidos/src/test/java/br/com/fiap/pos_tech_adj/tech_challenge_fase4/ms_gestao_pedidos/adapters/in/controller/PedidoControllerTest.java
@@ -12,11 +12,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,26 +24,27 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(PedidoController.class)
 @ExtendWith(MockitoExtension.class)
 class PedidoControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private CriarPedidoUseCase criarPedidoUseCase;
 
-    @MockBean
+    @Mock
     private ConsultarPedidoUseCase consultarPedidoUseCase;
 
-    @MockBean
+    @Mock
     private AtualizarStatusPedidoUseCase atualizarStatusPedidoUseCase;
 
-    @MockBean
+    @Mock
     private PedidoMapper pedidoMapper;
 
-    @Autowired
+    @InjectMocks
+    private PedidoController pedidoController;
+
+    @Mock
     private ObjectMapper objectMapper;
 
     private PedidoDTO pedidoDTO;
@@ -53,70 +52,51 @@ class PedidoControllerTest {
 
     @BeforeEach
     void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(pedidoController).build();
         pedidoId = UUID.randomUUID();
         pedidoDTO = new PedidoDTO();
         pedidoDTO.setId(pedidoId);
         pedidoDTO.setStatus("PENDENTE");
-       
-
+        objectMapper = new ObjectMapper();
     }
+
 
     @Test
     void testCriarPedido() throws Exception {
-
-        PedidoStatus status = PedidoStatus.PENDENTE;
-        
-        Pedido pedido = new Pedido();
-        pedido.setId(pedidoId);
-        pedido.setStatus(status); 
-
+        Pedido pedido = createPedido();
         when(criarPedidoUseCase.executar(any(PedidoDTO.class))).thenReturn(pedido);
         when(pedidoMapper.toDTO(pedido)).thenReturn(pedidoDTO);
 
         mockMvc.perform(post("/api/pedidos")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(pedidoDTO)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pedidoDTO)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(pedidoId.toString()))
-                // Comparando com a representação em String do enum (geralmente o nome do valor)
-                .andExpect(jsonPath("$.status").value(status.toString()));
+                .andExpect(jsonPath("$.status").value(PedidoStatus.PENDENTE.toString()));
 
         verify(criarPedidoUseCase, times(1)).executar(any(PedidoDTO.class));
     }
 
     @Test
     void testAtualizarStatusPedido() throws Exception {
-        // Supondo que PedidoStatus é um enum, ex: PENDENTE, CONFIRMADO, etc.
-        PedidoStatus status = PedidoStatus.PENDENTE;
-        
-        // Criar um objeto Pedido para retornar no mock
-        Pedido pedido = new Pedido();
-        pedido.setId(pedidoId);
-        pedido.setStatus(status);
-        
-        // Configuração dos mocks: agora usamos status.toString() para passar uma String
-        when(atualizarStatusPedidoUseCase.executar(eq(pedidoId), eq(status.toString()))).thenReturn(pedido);
+        Pedido pedido = createPedido();
+        when(atualizarStatusPedidoUseCase.executar(eq(pedidoId), eq(PedidoStatus.PENDENTE.toString()))).thenReturn(pedido);
         when(pedidoMapper.toDTO(pedido)).thenReturn(pedidoDTO);
-        
-        // Executa a requisição PUT, enviando o status como String
-        mockMvc.perform(put("/api/pedidos/atualizar-status")
-                .param("pedidoId", pedidoId.toString())
-                .param("status", status.toString()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(status.toString()));
-        
-        // Verifica se o use case foi chamado corretamente, usando status.toString() como parâmetro.
-        verify(atualizarStatusPedidoUseCase, times(1)).executar(eq(pedidoId), eq(status.toString()));
-    }
 
+        mockMvc.perform(put("/api/pedidos/atualizar-status")
+                        .param("pedidoId", pedidoId.toString())
+                        .param("status", PedidoStatus.PENDENTE.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(PedidoStatus.PENDENTE.toString()));
+
+        verify(atualizarStatusPedidoUseCase, times(1)).executar(eq(pedidoId), eq(PedidoStatus.PENDENTE.toString()));
+    }
 
     @Test
     void testConsultarPedido() throws Exception {
-        Pedido pedido = new Pedido(); 
-        pedido.setId(pedidoId); 
-
-        when(consultarPedidoUseCase.executar(pedidoId)).thenReturn(pedido); 
-        when(pedidoMapper.toDTO(pedido)).thenReturn(pedidoDTO); 
+        Pedido pedido = createPedido();
+        when(consultarPedidoUseCase.executar(pedidoId)).thenReturn(pedido);
+        when(pedidoMapper.toDTO(pedido)).thenReturn(pedidoDTO);
 
         mockMvc.perform(get("/api/pedidos/{id}", pedidoId))
                 .andExpect(status().isOk())
@@ -127,16 +107,21 @@ class PedidoControllerTest {
 
     @Test
     void testListarPedidos() throws Exception {
-        List<Pedido> pedidos = List.of(new Pedido()); 
-
-        when(consultarPedidoUseCase.executarTodos()).thenReturn(pedidos); 
-        when(pedidoMapper.toDTO(any(Pedido.class))).thenReturn(pedidoDTO); 
+        List<Pedido> pedidos = List.of(createPedido());
+        when(consultarPedidoUseCase.executarTodos()).thenReturn(pedidos);
+        when(pedidoMapper.toDTO(any(Pedido.class))).thenReturn(pedidoDTO);
 
         mockMvc.perform(get("/api/pedidos"))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.length()").value(1));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1));
 
         verify(consultarPedidoUseCase, times(1)).executarTodos();
     }
 
+    private Pedido createPedido() {
+        Pedido pedido = new Pedido();
+        pedido.setId(pedidoId);
+        pedido.setStatus(PedidoStatus.PENDENTE);
+        return pedido;
+    }
 }
